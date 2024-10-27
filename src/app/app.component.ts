eeb1674c91652f6f252acc5dfdda7cf7cd6a68d9
Jennilee Benda <jennileerbenda@gmail.com>
2024-10-27 01:48:26 -0500

    finally got routing figured out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLinkActive, RouterOutlet, RouterLink } from '@angular/router';
 import { Logo } from './logo/logo.component'
-import * as choirData from '../../data.json';
-import { ChorusDataForTable } from './types';
-import { ViewAll } from './viewAll/view-all.component';
 
 
 @Component({
@@ -12,27 +8,13 @@ import { ViewAll } from './viewAll/view-all.component';
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     Logo,
-    CommonModule,
-    ViewAll],
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'chorus-connector';
-  data: any[] = choirData.choruses;
-  @Input() savedData: ChorusDataForTable[] = []
-  ngOnInit() {
-    this.data.forEach((choir) => {
-      this.savedData.push({
-        id: choir.id,
-        name: choir.name,
-        description: choir.description,
-        contactEmail: choir.contactEmail,
-        locationCity: choir.location.city,
-        locationState: choir.location.state
-      })
-    })
-    // console.log(this.savedData)
-  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'search',
   standalone: true,
+  imports: [ RouterOutlet ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
